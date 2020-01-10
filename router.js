07ed1753dc4d328f8c774b97912cceb738280d5c
Katie Vogel <kvog983@gmail.com>
2020-01-09 21:45:03 -0500

    create router

---
+++ router.js
@@ -0,0 +1,8 @@
+var db = require("./models");
+require("./config/connection.js");
+const router = require("express").Router();
+const restaurantCtlr= require("./controllers/restaurantCtlr.js");
+
+router
+  .route("/api/restaurants")
+  .get(restaurantCtlr.findAll);
