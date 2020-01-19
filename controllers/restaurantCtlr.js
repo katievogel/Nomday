9b628a1b929c22ddbb471fc483de47ce4280967d
Katie Vogel <kvog983@gmail.com>
2020-01-19 13:27:31 -0500

    controller updates

--- controllers/restaurantCtlr.js
+++ controllers/restaurantCtlr.js
@@ -8,10 +8,27 @@ module.exports = {
             res.render("index", { fave_lunch_db: [] });
         });
     },
-
+//find a restaurant already in the database, if it does not exist, give option to add. if it does exist, update with latest visit date and add new record with rating to lunch spots db.  Add new entry to user db new or not.
     findOneAndUpdate: function (app) {
         app.post("/add_spot", function (req, res){
-            
-        })
+            db.LunchSpot.findOne({ where: { place_name: req.body.restaurant }}).then(function(dbLunchSpot){
+                var addRestToUserDB = dbLunchSpot.place_name;
+
+                addRestToUserDB(req.body.place_name, addRestToUserDB);
+
+                db.Users.findAll().then(function(tableRes){
+                    res.render("index", {
+                        lunchSpots: [dbLunchSpot.dataValues],
+                        Users: tableRes 
+                    });
+                });
+            });
+        });
+        function addNewRestToDB(restaurant, addRestToUserDB) {
+            db.Users.create({
+                restaurant: place_name,
+
+            })
+        }
     }
 };
