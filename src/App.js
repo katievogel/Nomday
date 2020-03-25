c934e341dae40777004fa1241b30aad191ee2593
Katie Vogel <kvog983@gmail.com>
2020-03-24 22:43:30 -0400

    component route addition of login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import CreateAccount from './pages/createAcc';
+import LogIn from './pages/login';
 
 function App() {
   return (
     <Router>
       <span className="">
         <Switch>
+          <Route exact path="/login/" component={ LogIn }/>
           <Route exact path="/createaccount/" component={ CreateAccount }/>
         </Switch>
       </span>
